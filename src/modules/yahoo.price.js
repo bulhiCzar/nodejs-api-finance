5d170ba605a83a0c7368ad2886c7e2f7b7906a5f
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-12-31 13:15:09 +0300

    Update yahoo.price.js

--- src/modules/yahoo.price.js
+++ src/modules/yahoo.price.js
@@ -2,13 +2,11 @@ const fetch = require('node-fetch');
 
 const YahooPrice = async (id) => {
     const url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${id}?modules=price`
-    // console.log("USAStockGetName. url для %s: %s", ID, url);
     try {
         const response = await fetch(url)
         const json = await response.json()
         const value = json.quoteSummary.result[0].price.regularMarketPrice.raw
-        // console.log("USAStockGetName. Название для %s: %s", ID, value)
-        if (value == 0) return 'нет'
+        if (!value) return 0
         return value
     } catch (e) {
         console.log('Ошибка в USAStockGetName')
