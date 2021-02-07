b2846e68ab8b2a580a584c87a69b1b0a9462edb2
bulhi <kp.bulhi@ya.ru>
2021-02-07 21:12:15 +0300

    Update database.yahoo.js

--- src/routes/database.yahoo.js
+++ src/routes/database.yahoo.js
@@ -34,25 +34,28 @@ route.get(
 
 
 route.post(
-    '/price',
+    '/price/:id',
     async (req, res) => {
         try {
-            const body = req.body
-            console.log(req)
-            return res.status(200).json({data: ['as']})
-            //
-            // const list = listId.split(',')
-            // const arr = []
-            //
-            // for (let i=0; i < list.length; i++){
-            //     const token = list[i]
-            //     const res = await YahooPrice(token)
-            //     arr.push(res)
-            // }
+            const listId = req.params.id
+            const list = listId.split(',')
+            const arr = []
+
+            for (let i=0; i < list.length; i++){
+                try {
+                    const token = list[i]
+                    const res = await YahooPrice(token)
+                    arr.push(res)
+                } catch (e) {
+                    arr.push('error')
+                }
+            }
+            console.log(arr)
 
 
             res.status(200).json({data: arr})
         } catch (e) {
+            console.log(e)
             res.status(403).json({data: 'Ошибка какая-то, повторите позже'})
         }
     }
